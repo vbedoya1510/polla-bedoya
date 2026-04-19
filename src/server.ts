fed556c0e890c92212a19d57a0a358cb996e23d5
victor <victorbedoya1510@gmail.com>
2026-04-19 11:27:47 -0500

    Prueba despliegue

--- src/server.ts
+++ src/server.ts
@@ -66,3 +66,5 @@ if (isMainModule(import.meta.url) || process.env['pm_id']) {
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
 export const reqHandler = createNodeRequestHandler(app);
+
+export default app;
